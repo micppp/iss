922fc447a28c436ac1003afe671a267de67abf17
Michael Pallister <micp@Michaels-MBP.lan>
2019-01-30 21:25:50 +0000

    Updating readme and files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Map from "pigeon-maps";
+import Marker from "pigeon-marker";
 
 class App extends Component {
+  state = {
+    latitude: "",
+    loading: true,
+    longitude: ""
+  };
+
+  getLocation = () =>
+    axios
+      .get("https://api.wheretheiss.at/v1/satellites/25544.json")
+      .then(response =>
+        this.setState({
+          ...response.data,
+          loading: false
+        })
+      );
+
+  componentDidMount = () => this.getLocation();
+
   render() {
+    const { latitude, loading, longitude } = this.state;
+
+    const map = (
+      <Map
+        center={[latitude - 20, longitude - 10]}
+        zoom={2}
+        width={450}
+        height={400}
+      >
+        <Marker anchor={[latitude, longitude]} payload={1} />
+      </Map>
+    );
+
+    const loader = (
+      <div className="lds-ripple">
+        <div />
+        <div />
+      </div>
+    );
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={loading ? "app app--loading" : "app"}>
+        {loading ? (
+          loader
+        ) : (
+          <>
+            <h1 className="app__h1">ISS Location</h1>
+            <p>
+              <strong>Lat:</strong> {latitude},<strong>&nbsp;Lng:</strong>{" "}
+              {longitude}
+            </p>
+            <div className="app-map">{map}</div>
+            <button className="app-button" onClick={() => this.getLocation()}>
+              Update location
+            </button>
+          </>
+        )}
       </div>
     );
   }
